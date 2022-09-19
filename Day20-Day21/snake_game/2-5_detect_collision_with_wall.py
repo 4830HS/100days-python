63a9bcbba06af749551e072fdcbcdc408e5088c8
4830hs <4830hs@gmail.com>
2022-09-19 16:30:42 +0900

    100days python

--- Day20-Day21/snake_game/2-5_detect_collision_with_wall.py
+++ Day20-Day21/snake_game/2-5_detect_collision_with_wall.py
@@ -1,8 +1,11 @@
-# TODO 2-2 : Refactoring(oop)
+# TODO 2-4 : Create a scoreboard
 
-from turtle import Screen, Turtle
+from turtle import Screen
+# 더이상 Turtle은 사용하지 않으니 'Turtle'은 import 항목에서 지워주기
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width = 600, height = 600)
@@ -11,6 +14,8 @@ screen.title("Alexnake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -22,7 +27,12 @@ game_is_on = True
 while game_is_on :
     screen.update()
     time.sleep(0.1)
-
     snake.move()
    
+    # Detect collision with food.
+    if snake.head.distance(food) < 13 :
+        # food의 크기가 10x10이므로
+        food.refresh()
+        scoreboard.increase_score()
+
 screen.exitonclick()
