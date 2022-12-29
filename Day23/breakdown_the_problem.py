1f26c714dda02d2fecf020694f78d3ef92c3ad4a
4830hs <4830hs@gmail.com>
2022-12-29 22:52:25 +0000

    100 days python

---
+++ Day23/breakdown_the_problem.py
@@ -0,0 +1,8 @@
+# 1. Move the turtle with keypress
+
+# 2. Create and move the cars
+# 높이 20픽셀, 너비 40픽셀인 자동차들을 y축 범위 내에서 무작위로 생성하고, 화면의 왼쪽 가장자리로 움직이세요. 자동차는 화면의 위아래 가장자리에서 50픽셀 이상 띄워서 생성해야 합니다(거북이를 위한 안전지대라고 생각하세요). 힌트) 게임 반복문이 6번 실행될 때마다 자동차를 새로 생성하세요. 풀다가 막히면 4단계 설명 영상을 확인해 보세요.
+
+# 3. Detect collision with car
+# 4. Detect when turtle reaches the other side
+# 5. Create a scroeboard
